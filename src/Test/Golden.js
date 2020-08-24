b154a65616c5fc1f11467701157a786e357879af
Kaden Thomas <thomas.kaden4@outlook.com>
2020-08-23 20:07:37 -0700

    Clean files before diffing

--- src/Test/Golden.js
+++ src/Test/Golden.js
@@ -1,7 +1,11 @@
 const diff = require("diff");
 
+function clean(text) {
+  return text.split(/\r?\n/g).join("\n");
+}
+
 function diffImpl(first) {
-  return second => diff.diffLines(first, second).filter(x => x.added || x.removed);
+  return second => diff.diffLines(clean(first), clean(second)).filter(x => x.added || x.removed);
 }
 
 exports.diffImpl = diffImpl;
